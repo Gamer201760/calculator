d63cb697d483b8fc34c85fcb1d56567ef3af8564
Azamat <azamat201760@yandex.ru>
2025-09-20 13:24:19 +0300

    simple polish

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+def calc(expr: str) -> list:
+    stack = []
+    for x in expr.split():
+        if x.isdigit():
+            stack.append(int(x))
+        elif x == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif x == "-":
+            stack.append(stack.pop() - stack.pop())
+        elif x == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif x == "/":
+            stack.append(stack.pop() // stack.pop())
+    return stack
+
+
 def main():
-    print("Hello from calc!")
+    print(calc("2 2 2 + /"))
 
 
 if __name__ == "__main__":
