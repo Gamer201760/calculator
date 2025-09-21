2fcd85a867bd8a4f9d8e4bac9f79fc9cf644437c
Azamat <azamat201760@yandex.ru>
2025-09-21 12:21:57 +0300

    [fix] test rpn calculator, init rpn converter

--- test/test_rpn_calc_usecase.py
+++ test/test_rpn_calc_usecase.py
@@ -1,4 +1,5 @@
 from repository.operator import OperatorRepository
+from repository.rpn_converter import RPNConverter
 from repository.token_parser import SpaceTokenizer
 from usecase.rpn_calculator import RPNCalculatorUseCase
 
@@ -6,6 +7,7 @@ from usecase.rpn_calculator import RPNCalculatorUseCase
 def test_rpn_calculator_usecase():
     op = OperatorRepository()
     parser = SpaceTokenizer(op)
-    calc = RPNCalculatorUseCase(parser)
+    converter = RPNConverter()
+    calc = RPNCalculatorUseCase(parser, converter)
     ans = calc.calculate('3 4 + 10 *')
     assert ans == 70
